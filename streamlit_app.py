406b60e80d6784136f7e4bc915260570fa268e8c
Diná Beatriz Xavier <115733424+dinaprodap@users.noreply.github.com>
2024-09-10 18:18:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ if st.button('Calcular'):
     # Tabela com os dados
     resultados_df = pd.DataFrame({
         'Descrição': ['Carga Unidade Animal', 'Animais/ha', 'Animais/ha Suportado'],
-        'Valor': [f"{capacidade_suporte:.2f} UA/ha", f"{carga_atual:.2f} UA/ha", 
+        'Valor': [f"{carga_atual:.2f} UA/ha", 
                   f"{animais_por_hectare_atual:.2f}", f"{animais_por_hectare_suportado:.2f}"]
     })
     st.dataframe(resultados_df)
